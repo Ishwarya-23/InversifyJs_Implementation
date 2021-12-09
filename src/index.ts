4dbb3cf70dcfd4b1e0f74a1fb23154b17e7a9d62
Ishwarya-23 <mailtoash89@gmail.com>
2021-12-09 00:17:53 -0500

    Code cleanup
    Added error handling and some comments

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { container } from "./inversify.config";
 import { ApiManager } from "./utils/apiManager";
 
 let apiManager = container.get<ApiManager>("ApiManager");
-apiManager.fetchData();
+apiManager.fetchData().then(response => console.log(response)).catch(console.error);
--- src/utils/apiManager.ts
+++ src/utils/apiManager.ts
@@ -5,10 +5,16 @@ import "reflect-metadata";
 @injectable()
 export class ApiManager {
     private todoClient: ITodo;
+
+    // Injecting todoClient dependency instead of creating it
     constructor(@inject("Todo") todoClient:ITodo) {
         this.todoClient = todoClient;
     }
 
+    /**
+     * Calls the fetchData function from the injected class
+     * @returns a promise
+     */
     fetchData = async () : Promise<any> => {
         return await this.todoClient.fetchData();
     }
